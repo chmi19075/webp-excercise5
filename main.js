83b6610ecc2e664b88779a723387a63935b2a44b
chmi19075 <78012962+chmi19075@users.noreply.github.com>
2021-01-30 19:30:24 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -15,29 +15,29 @@ let insertZ = ['spontaneously combusted','melted into a puddle on the sidewalk',
 randomize.addEventListener('click', result);
 
 function result() {
-    let newStory = storyText;
+  let newStory = storyText;
 
-    let xItem = randomValueFromArray(insertX);
-    let yItem = randomValueFromArray(insertY);
-    let zItem = randomValueFromArray(insertZ);
+  let xItem = randomValueFromArray(insertX);
+  let yItem = randomValueFromArray(insertY);
+  let zItem = randomValueFromArray(insertZ);
 
-    newStory = newStory.replace(':insertx:',xItem);
-    newStory = newStory.replace(':insertx:',xItem);
-    newStory = newStory.replace(':inserty:',yItem);
-    newStory = newStory.replace(':insertz:',zItem);
+  newStory = newStory.replace(':insertx:', xItem);
+  newStory = newStory.replace(':inserty:', yItem);
+  newStory = newStory.replace(':insertz:', zItem);
+  newStory = newStory.replace(':insertx:', xItem);
 
-    if(customName.value !== '') {
-      const name = customName.value;
-      newStory = newStory.replace('Bob',name);
-    }
+  if(customName.value !== '') {
+    let name = customName.value;
+    newStory = newStory.replace('Bob', name);
+  }
 
-    if(document.getElementById("uk").checked) {
-        const weight = Math.round(300*0.0714286) + ' stone';
-        const temperature =  Math.round((94-32) * 5 / 9) + ' centigrade';
-        newStory = newStory.replace('94 fahrenheit',temperature);
-        newStory = newStory.replace('300 pounds',weight);
-    }
-    
-    story.textContent = newStory;
-    story.style.visibility = 'visible';
+  if(document.getElementById("uk").checked) {
+    let weight = Math.round(300 * 0.0714286) + ' stone';
+    let temperature =  Math.round((94 - 32) * 5/9) + ' centigrade';
+    newStory = newStory.replace('94 fahrenheit', temperature);
+    newStory = newStory.replace('300 pounds', weight);
+  }
+
+  story.textContent = newStory;
+  story.style.visibility = 'visible';
 }
